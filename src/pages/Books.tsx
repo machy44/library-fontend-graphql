92e544d667397977e9b7a6791d79d36b3ed12081
alen.sljivar <alen.sljivar@five.agency>
2022-07-10 23:59:16 +0200

    update books table ui

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
 import { IBook } from '../types';
 import { Card } from '../ui/Card';
+import { Table, THead, Tr, Th, Tbody, Td } from '../ui/Table';
 import { Title } from '../ui/Text';
 
 const Books: React.FC = () => {
@@ -9,24 +10,25 @@ const Books: React.FC = () => {
 
   return (
     <Card>
-      <Title>books</Title>
-
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>author</th>
-            <th>published</th>
-          </tr>
+      <Title className="text-center">books</Title>
+      <Table>
+        <THead>
+          <Tr>
+            <Th />
+            <Th>author</Th>
+            <Th>published</Th>
+          </Tr>
+        </THead>
+        <Tbody>
           {data?.allBooks.map((a) => (
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author}</td>
-              <td>{a.published}</td>
-            </tr>
+            <Tr key={a.title}>
+              <Td>{a.title}</Td>
+              <Td>{a.author}</Td>
+              <Td>{a.published}</Td>
+            </Tr>
           ))}
-        </tbody>
-      </table>
+        </Tbody>
+      </Table>
     </Card>
   );
 };
