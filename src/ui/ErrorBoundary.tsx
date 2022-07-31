97ab204d7cb70dad84301007f1369dd520ed740d
alen.sljivar <alen.sljivar@five.agency>
2022-07-31 22:55:47 +0200

    update error boundary

--- src/ui/ErrorBoundary.tsx
+++ src/ui/ErrorBoundary.tsx
@@ -1,11 +1,10 @@
-import React, { ErrorInfo, ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 
-// @ts-ignore
-function ErrorFallback({ error }) {
+function ErrorFallback({ error }: { error: State['error'] }) {
   return (
     <div role="alert">
       <p>Something went wrong:</p>
-      <pre style={{ color: 'red' }}>{error.message}</pre>
+      <pre style={{ color: 'red' }}>{error?.message}</pre>
     </div>
   );
 }
@@ -16,19 +15,19 @@ interface Props {
 
 interface State {
   hasError: boolean;
-  error: string;
+  error: Error | undefined;
 }
 
 export class ErrorBoundary extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { hasError: false, error: '' };
+    this.state = { hasError: false, error: undefined };
   }
 
   static getDerivedStateFromError(error: Error) {
     // Update state so the next render will show the fallback UI.
     console.log({ error });
-    return { hasError: true, error };
+    return { hasError: true, error: error };
   }
   // componentDidCatch(error, errorInfo) {
   // You can also log the error to an error reporting service
