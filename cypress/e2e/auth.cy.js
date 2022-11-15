9b2f72ec1e8c7b833ca07a250d4b8d08a7c343e5
alen.sljivar <alen.sljivar@five.agency>
2022-11-15 09:54:39 +0100

    create cypress login testcase

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,14 +1,18 @@
 describe('Auth', function () {
   beforeEach(function () {
-    cy.visit('http://localhost:3000/login');
+    cy.visit('http://localhost:3000');
   });
 
   describe('LoginForm', function () {
     it('login to application', function () {
-      cy.contains('Notes');
-      cy.contains('Note app, Department of Computer Science, University of Helsinki 2022');
+      cy.get('[data-testid=login-menu-item]').click();
+      cy.get('[data-testid=username-input]').type('test');
+      cy.get('[data-testid=password-input]').type('test');
+      cy.get('[data-testid=login-submit]').click();
+      cy.contains('authors');
     });
     it('login should redirect when user is logged in already', function () {
+      // create login command
       cy.contains('Notes');
       cy.contains('Note app, Department of Computer Science, University of Helsinki 2022');
     });
