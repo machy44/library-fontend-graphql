d9a7e5827495c0c63c8a901ba04b839bb9bb3043
alen.sljivar <alen.sljivar@five.agency>
2022-11-18 15:46:24 +0100

    update auth testcase

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -5,7 +5,7 @@ describe('Auth', function () {
     beforeEach(function () {
       cy.visit('http://localhost:3000');
     });
-    it.only('login to application', function () {
+    it('login to application', function () {
       cy.getByTestId(Navigation.loginItem).click();
       cy.getByTestId(LoginForm.usernameInput).type('test');
       cy.getByTestId(LoginForm.passwordInput).type('test');
@@ -26,18 +26,18 @@ describe('Auth', function () {
       cy.get('[data-testid=login-submit]').click();
     });
 
-    it('login should redirect when user is logged in already', function () {
-      cy.get('[data-testid=login-menu-item]').click();
-      cy.get('[data-testid=username-input]').type('test');
-      cy.get('[data-testid=password-input]').type('test');
-      cy.get('[data-testid=login-submit]').click();
+    it.only('login should redirect when user is logged in already', function () {
+      cy.getByTestId(Navigation.loginItem).click();
+      cy.getByTestId(LoginForm.usernameInput).type('test');
+      cy.getByTestId(LoginForm.passwordInput).type('test');
+      cy.getByTestId(LoginForm.submit).click();
       cy.wait(1000);
-      expect(cy.get('[data-testid=author-table-body]')).to.exist;
-      expect(cy.get('[data-testid=author-edit-form]')).to.exist;
+      expect(cy.getByTestId(AuthorsPage.tableBody)).to.exist;
+      expect(cy.getByTestId(AuthorsPage.editForm)).to.exist;
       cy.visit('http://localhost:3000/login');
       cy.wait(1000);
-      expect(cy.get('[data-testid=author-table-body]')).to.exist;
-      expect(cy.get('[data-testid=author-edit-form]')).to.exist;
+      expect(cy.getByTestId(AuthorsPage.tableBody)).to.exist;
+      expect(cy.getByTestId(AuthorsPage.editForm)).to.exist;
     });
     // });
   });
