beea0091861a0e77fbd911a896601db8f6f2c945
alen.sljivar <alen.sljivar@five.agency>
2022-11-22 11:39:31 +0100

    add testcases which I have to do

--- cypress/e2e/authorsPage.cy.js
+++ cypress/e2e/authorsPage.cy.js
@@ -3,6 +3,11 @@ describe('authors page', () =>
     beforeEach(function () {
       cy.visit('http://localhost:3000');
     });
+
+    // shoulkd compare table and select options and they have to be same
+    // edit author should work
+    // table should be updated when author year is changed
+    // amount should be number testcase when you put text in input field
     it('update authors birth year', function () {
       cy.get('tbody > tr').first().contains('1952');
       cy.editAuthor({ name: 'Robert Martin', born: 1954 });
--- cypress/e2e/navigation.cy.js
+++ cypress/e2e/navigation.cy.js
@@ -1,1 +1,4 @@
 export {};
+// should have login books Authors
+// should have logout, addBook, RecommendBooks
+// should have edit authors in authors page when logged in
