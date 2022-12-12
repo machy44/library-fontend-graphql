381ec5497a6f9080d7b68d095285beef55a49e92
alen.sljivar <alen.sljivar@five.agency>
2022-12-12 22:32:00 +0100

    update info

--- src/ui/Info.tsx
+++ src/ui/Info.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Info = ({ content }: { content: React.ReactNode }) => {
   return content ? (
     <div
-      className="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3"
+      className="flex items-center bg-blue-500 text-white text-sm font-bold px-4 py-3 max-h-5"
       role="alert">
       <svg
         className="fill-current w-4 h-4 mr-2"
